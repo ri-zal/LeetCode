41fcd63940d31e4970a782b646f4a97b7c63adc3
rizal <116851791+ri-zal@users.noreply.github.com>
2023-04-21 14:32:34 -0700

    version without arrays/vectors, 8 lines less

--- 7 - Reverse Integer/main.cpp
+++ 7 - Reverse Integer/main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
-#include <vector>
-
 
 int main() {
     int x;
-    std::vector<int> x_parts;
+    int reversed_x = 0;
 
     std::cout << "Enter integer to be reversed: ";
     std::cin >> x;
 
     while (x != 0) {
-        int temp = x % 10;
+        reversed_x = (reversed_x * 10) + (x % 10);
         x /= 10;
-        x_parts.push_back(temp);
     }
 
-    for (int x_part : x_parts) {
-        std::cout << x_part;
-    }
-
-    std::cout << "\n";
+    std::cout << reversed_x << std::endl;
 
     return 0;
 }
