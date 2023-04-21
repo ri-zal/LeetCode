c35979d8ab6c293242a8a6c4ee13b117b8c226c5
rizal <116851791+ri-zal@users.noreply.github.com>
2023-04-21 14:10:25 -0700

    Create main.cpp

---
+++ 7 - Reverse Integer/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+
+int main() {
+    int x;
+    std::vector<int> x_parts;
+    int index = 0;
+
+    std::cout << "Enter integer to be reversed: ";
+    std::cin >> x;
+
+    while (x != 0) {
+        int temp = x % 10;
+        x /= 10;
+        x_parts.push_back(temp);
+    }
+
+    for (int x_part : x_parts) {
+        std::cout << x_part;
+    }
+
+    std::cout << "\n";
+
+    return 0;
+}
